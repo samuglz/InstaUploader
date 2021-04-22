80e17e015fcc32893d97b578c1d0106c1d6a384e
Samuel Gonzalez <samuelgonzalezalonso@gmail.com>
2021-04-22 19:49:36 +0100

    WiP: Added vue-router

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,7 @@
 <template>
     <div id="app">
-        <main-view />
+        <router-view />
     </div>
 </template>
-
-<script>
-import MainView from '@/views/MainView';
-export default {
-    name: 'App',
-    components: { MainView }
-};
-</script>
 
 <style></style>
