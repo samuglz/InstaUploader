466d257094f53c1554f30f25a7d4b84df23bb1ca
Samuel Gonzalez <samuelgonzalezalonso@gmail.com>
2021-04-22 23:45:43 +0100

    WiP: center grid and add redirect

--- frontend/src/views/Gallery.vue
+++ frontend/src/views/Gallery.vue
@@ -1,16 +1,19 @@
 <template>
-    <div class="h-screen">
+    <div class="">
         <logo />
-        <div class="gallery overflow-y-auto ml-10">
+        <div
+            class="gallery overflow-y-auto w-full flex justify-center items-center gap-3"
+        >
             <div v-for="file in files" :key="file.name" class="w-full">
                 <img
                     :src="getUrl(file)"
                     :alt="file.name"
                     :style="{
                         width: '300px',
-                        'box-shadow': '2px 2px 2px 4px rgba(0,0,0,0.1)'
+                        'box-shadow': '0 0 0px 2px rgba(0,0,0,0.1)'
                     }"
                     class="rounded-md cursor-pointer"
+                    @click="goToImage(file.name)"
                 />
             </div>
         </div>
@@ -40,6 +43,9 @@ export default {
     methods: {
         getUrl(value) {
             return `http://localhost:7000${value.url}`;
+        },
+        goToImage(value) {
+            this.$router.push(`/image/${value}`);
         }
     }
 };
@@ -49,6 +55,5 @@ export default {
 .gallery {
     display: grid;
     grid-template-columns: repeat(auto-fit, 300px);
-    gap: 10px;
 }
 </style>
