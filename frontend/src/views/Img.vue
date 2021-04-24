6a22b70e5b325aeddbd1183e25bcad0eb029eab4
Samuel Gonzalez <samuelgonzalezalonso@gmail.com>
2021-04-24 23:08:55 +0100

    WiP: add width to images

--- frontend/src/views/Img.vue
+++ frontend/src/views/Img.vue
@@ -8,7 +8,7 @@
             <img
                 :src="img.url"
                 :alt="img.name"
-                class="rounded-md"
+                class="rounded-md w-1/3"
                 :style="{ 'box-shadow': '0 0 10px 2px rgba(0,0,0,0.1)' }"
             />
         </div>
