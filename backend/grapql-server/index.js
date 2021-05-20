f5ba1266f1d2b43658619932641bafaa7a33d407
Samuel Gonzalez <samuelgonzalezalonso@gmail.com>
2021-05-20 22:48:03 +0100

    add environment variables, prettierrc and graphql querys

--- backend/grapql-server/index.js
+++ backend/grapql-server/index.js
@@ -1,3 +1,30 @@
-import { ApolloServer, gql } from "apollo-server";
+import { ApolloServer, gql } from 'apollo-server';
+import Axios from 'axios';
+import { config } from 'dotenv';
+config();
 
-const typeDefs = gql``;
+const typeDefs = gql`
+    type Image {
+        name: String
+        url: String
+    }
+    type Query {
+        images: [Image]
+    }
+`;
+
+const resolvers = {
+    Query: {
+        images: async () => {
+            const { data } = await Axios.get(`${process.env.REST_HOST}/images`);
+            return data;
+        }
+    }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+});
